e061a3191f68a60d4083fc87061449d19be7c7a0
PhimphixayNouhak <140489606+PhimphixayNouhak@users.noreply.github.com>
2024-08-02 21:05:33 +0700

    Add files via upload

---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        private WebsiteBanHangDbContext db = new WebsiteBanHangDbContext();
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            ViewBag.CountOrderSuccess = db.Orders.Where(m => m.Status == 3).Count();
+            ViewBag.CountOrderCancel = db.Orders.Where(m => m.Status == 1).Count();
+            ViewBag.CountContactDoneReply = db.Contacts.Where(m => m.Flag == 0).Count();
+            var list = db.Orders.Where(x => x.Status == 3).Select(x => x.ID).ToList();
+            ViewBag.Total = db.Orderdetails.Where(x => list.Contains(x.OrderID)).Sum(m => m.Amount);
+            ViewBag.CountUser = db.Products.Where(m => m.Status != 0).Count();
+            return View();
+        }
+    }
+}
